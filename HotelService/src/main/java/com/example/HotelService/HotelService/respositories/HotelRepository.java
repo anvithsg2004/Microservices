c8f6e7e62024ca8a18800d3a6824bc7961eed18d
Anvith S G <anvithsg2004@gmail.com>
2025-05-17 22:02:39 +0530

    Starting from starting

--- HotelService/src/main/java/com/example/HotelService/HotelService/respositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.HotelService.HotelService.respositories;
-
-import com.example.HotelService.HotelService.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
--- UserService/src/main/java/com/example/UserService/UserService/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.UserService.UserService.repository;
-
-import com.example.UserService.UserService.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
