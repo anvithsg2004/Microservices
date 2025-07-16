f9729a7c73b6dfb928ecaf411f7573e9ea3c573f
Anvith S G <anvithsg2004@gmail.com>
2025-07-16 22:10:22 +0530

    Tried to add Auth

---
+++ ApiGateway/src/main/java/com/gateway/ApiGateway/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.gateway.ApiGateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults()) // Enable OAuth2 Login
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return http.build();
+    }
+}
